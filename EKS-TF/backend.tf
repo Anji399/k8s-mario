b090766919c3ad8600afed3b4658e474828ec202
Prasanna399 <prasanna@kiyocrm.com>
2024-01-16 21:09:07 +0530

    first commit

---
+++ EKS-TF/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "super-mario-bucket-1717" # Replace with your actual S3 bucket name
+    key    = "EKS/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
